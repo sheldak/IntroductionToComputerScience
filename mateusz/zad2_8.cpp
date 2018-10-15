ab7b52b13b06053f24fd374b75eea792aa3f5064
mateuszmonas <mateuszmonas@gmail.com>
2018-10-15 13:51:53 +0200

    zad2_7

--- mateusz/zad2_8.cpp
+++ mateusz/zad2_8.cpp
@@ -1,6 +1,19 @@
-void zad2_8(double a, double b, unsigned long n){
-    double result = a/b;
-    string r = to_string(result);
-    unsigned long z = r.find('.');
-    cout << r.substr(0, z+n+1);
+void zad2_8(int a, int b, unsigned long n){
+    string str;
+    n++;
+    str += to_string(a/b);
+    str+='.';
+    a = a%b;
+    a*=10;
+    for (int i = 0; i < n; ++i) {
+        //w pewnym momenicie a moze ovrflow, wiec returnujemy
+        if(a<0){
+            cout << "nie da sie";
+            return;
+        }
+        str += to_string(a/b);
+        a = a%b;
+        a*=10;
+    }
+    cout << str;
 }
