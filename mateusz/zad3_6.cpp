f8af23a1ba3a059a18d1d7a951e98bb18ff2ad84
mateuszmonas <mateuszmonas@gmail.com>
2018-10-19 21:02:02 +0200

    zad 3_6

--- mateusz/zad3_6.cpp
+++ mateusz/zad3_6.cpp
@@ -1,7 +1,25 @@
-void zad3_6(int a, int b){
-    int a_length, b_length;
-    int i;
-    while (i!=0){
-        cin >> i;
+void zad3_6(){
+    int n = 0;
+    // initialize array as zeros so it does not have random values
+    int numbers[10] = {0,0,0,0,0,0,0,0,0,0};
+    // read values until 0 is encountered
+    while (true){
+        cin >> n;
+        if(n==0){
+            break;
+        }
+        // we check each array element until we find index where i-1 is bigger and i is smaller than n
+        for (int i = 9; i >= 0; --i) {
+            // by using hard comparison we avoid inserting duplicates into the array
+            if (n>numbers[i] && (i-1<0 || n<numbers[i-1])){
+                // we move all elements smaller than n back
+                for (int j = 8; j >= i; --j) {
+                    numbers[j+1]=numbers[j];
+                }
+                numbers[i] = n;
+                break;
+            }
+        }
     }
+    cout << numbers[9];
 }
