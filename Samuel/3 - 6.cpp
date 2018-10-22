170be2f586e6b5afc2746118f16795c757afd898
sheldak <sheldak@poczta.fm>
2018-10-22 18:33:42 +0200

    Update 3 - 6.cpp

--- Samuel/3 - 6.cpp
+++ Samuel/3 - 6.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 using namespace std;
 void c3_6(){
-    const int N = 10000;
-    int end = -1;
+    int eend = -1;
     int len = 0;
-    int tab[N];
 
-    int max[10];
+    int maxx[10];
     for(int i=0; i<10; i++)
-        max[i] = 0;
+        maxx[i] = 0;
 
-    while(end != 0){
-        cin>>tab[len];
+    while(eend != 0){
+        int n;
+        cin>>n;
 
-        if(tab[len] == 0)
-            end = 0;
+        if(n == 0)
+            eend = 0;
 
         int next = -1;
 
         for(int i=0; i<10; i++){
-            if(tab[len] >= max[i] && next < 0){
-                next = max[i];
-                max[i] = tab[len];
+            if(n >= maxx[i] && next < 0){
+                next = maxx[i];
+                maxx[i] = n;
             }
-            else if(next > 0 && next > max[i]){
-                int k = max[i];
-                max[i] = next;
+            else if(next > 0 && next > maxx[i]){
+                int k = maxx[i];
+                maxx[i] = next;
                 next = k;
             }
         }
@@ -33,5 +32,5 @@ void c3_6(){
         len++;
     }
 
-    cout<<max[9];
+    cout<<maxx[9];
 }
