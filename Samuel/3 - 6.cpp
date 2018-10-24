4451965b3689ca08ebd298e2199d7c340dc703ec
sheldak <sheldak@poczta.fm>
2018-10-24 09:30:29 +0200

    Update 3 - 6.cpp

--- Samuel/3 - 6.cpp
+++ Samuel/3 - 6.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
 using namespace std;
 void c3_6(){
-    int eend = -1;
-    int len = 0;
+    bool eend = false;
 
     int maxx[10];
     for(int i=0; i<10; i++)
         maxx[i] = 0;
 
-    while(eend != 0){
+    while(!eend){
         int n;
         cin>>n;
 
         if(n == 0)
-            eend = 0;
+            eend = true;
 
         int next = -1;
 
-        for(int i=0; i<10; i++){
-            if(n >= maxx[i] && next < 0){
-                next = maxx[i];
-                maxx[i] = n;
-            }
-            else if(next > 0 && next > maxx[i]){
-                int k = maxx[i];
-                maxx[i] = next;
-                next = k;
+        if(n>maxx[9]){
+            for(int i=0; i<10; i++){
+                if(n >= maxx[i] && next < 0){
+                    next = maxx[i];
+                    maxx[i] = n;
+                }
+                else if(next > 0 && next > maxx[i]){
+                    int k = maxx[i];
+                    maxx[i] = next;
+                    next = k;
+                }
             }
         }
-
-        len++;
     }
 
     cout<<maxx[9];
