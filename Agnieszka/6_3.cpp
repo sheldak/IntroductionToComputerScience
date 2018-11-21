c398dbe8ae6d86a60c41438ce27ebfc6c6537317
agga1 <agud100@gmail.com>
2018-11-21 12:49:44 +0100

    Add files via upload

---
+++ Agnieszka/6_3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+const int N = 6;
+bool odwaz(int t[N], int masa, int frst=0){
+    if(frst==N){
+        return false;
+    }
+    for(int i=frst;i<N;i++){
+        if(masa-t[i]==0)  // masa zostala osiagnieta
+        {
+            cout<<t[i]<<" ";
+            return true;
+        }
+        if(masa-t[i]<0)
+            continue;  // odwaznik za duzy
+        if(odwaz(t, masa-t[i], i+1)){
+            cout<<t[i]<<" ";
+            return true;
+        }
+    }
+    return false;
+}
+main(){
+    int t[]={2,3,5,8,9,23};
+    odwaz(t, 34);
+}
