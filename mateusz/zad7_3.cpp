ba8bab8b53f47963739870de1bb4b4a43d23b9b3
mateuszmonas <mateuszmonas@gmail.com>
2018-11-26 00:25:06 +0100

    zad7_3

--- mateusz/zad7_3.cpp
+++ mateusz/zad7_3.cpp
@@ -1,13 +1,26 @@
-int zad7_3(int t[N], int sum, int index_sum, int start){
-    int result = 0;
+void c(int t[N], int sum, int index_sum, int start, int &minimum, int curr, int &result){
+    if(curr>=minimum){
+        return;
+    }
     if(sum==index_sum){
-        result=max(result, sum);
+        if(curr!=0){
+            result=sum;
+            minimum=curr;
+        }
     }
     if(start==N){
-        return result;
+        return;
     }
     for (int i = start; i < N; ++i) {
-        result=max(result, zad7_3(t, sum+t[i], index_sum+i, i+1));
+        c(t, sum+t[i], index_sum+i, i+1, minimum, curr+1, result, a);
     }
+}
+
+int zad7_3(int t[N]){
+    int result = 0;
+    int min = N+1;
+    int a = 0;
+    c(t, 0, 0, 0, min, 0, result, a);
+    cout << result << " " << a << " " << min << " " << endl;
     return result;
 }
